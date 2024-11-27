af0f909217c5fab1d5295e28ebe4df03a0d2c051
prabhatsn1 <prabhatsn1@gmail.com>
2024-11-28 00:25:28 +0530

    Header and searchbar completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ConvexClientProvider>
+          <ClerkProvider>{children}</ClerkProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
